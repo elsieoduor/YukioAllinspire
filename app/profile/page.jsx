236b5ed37af2f71f5e8d707b1d3e47d05a24107c
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-12-06 13:13:36 +0300

    routes

---
+++ app/profile/page.jsx
@@ -0,0 +1,105 @@
+// 'use client';
+// import { useSession } from 'next-auth/react';
+// import { useRouter } from 'next/navigation';
+// import {useState, useEffect} from 'react'
+// import Profile from '@components/Profile';
+
+// const MyProfile = () => {
+//   const {data: session} = useSession();
+//   const [post, setPost] = useState([])
+//   const router = useRouter();
+
+//   useEffect(()=>{
+//     const fetchPosts = async ()=>{
+//       const response = await fetch(`/api/users/ ${session?.user.id}/posts`)
+//       const data = await response.json();
+//       setPost(data);
+//     }
+//     if (session?.user.id)fetchPosts();
+//   },[session?.user.id])
+//   const handleEdit = (post)=>{
+//     router.push(`/update-prompt?id=${post._id}`)
+//   }
+//   const handleDelete = async (post)=>{
+//     const hasConfirmed = confirm('Are you sure you want to delete?')
+//     if (!hasConfirmed){
+//       try{
+//         await fetch(`/api/prompt/${post._id.toString()}`,{
+//           method: 'DELETE'
+//         })
+//         const filteredPosts = post.filter((p)=>p._id !== post._id)
+//         setPost(filteredPosts)
+//       }catch(error){
+//         console.log(error)
+//       }
+//     }
+//   }
+//   return (
+//     <Profile  name='My' desc='Welcome to here' data={post} handleEdit={handleEdit} handleDelete={handleDelete}/>
+//   )
+// }
+
+// export default MyProfile
+
+
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
